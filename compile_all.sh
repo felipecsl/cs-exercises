48248c36b918cf95236fb1147a995fd3dd5f8ed5
Marcelo Juchem <juchem at gmail dot com>
2012-04-19 01:05:56 -0700

    avoiding include directory in compile_all.sh

--- compile_all.sh
+++ compile_all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for f in *; do
-	if [ "`basename "$f"`" != "partial" ] && [ "`basename "$f"`" != "tmp" ] && [ -d "$f" ]; then
+	if [ "`basename "$f"`" != "prep" ] &&  [ "`basename "$f"`" != "partial" ] && [ "`basename "$f"`" != "tmp" ] && [ -d "$f" ]; then
 		./go.sh "$f"
 		echo
 		echo "$f"
