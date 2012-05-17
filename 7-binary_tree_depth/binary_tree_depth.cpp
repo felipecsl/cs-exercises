1660c5bbd04bfbab05606fc78449c1e2aaed110c
Marcelo Juchem <juchem at gmail dot com>
2012-05-17 00:06:55 -0700

    simplifying code for exercise 7

--- 7-binary_tree_depth/binary_tree_depth.cpp
+++ 7-binary_tree_depth/binary_tree_depth.cpp
@@ -1,4 +1,5 @@
 #include "../prep/binary_tree.h"
+#include "../69-print_binary_tree/print_binary_tree.h"
 
 #include <iostream>
 
@@ -8,18 +9,20 @@ template <typename node_type>
 size_t binary_tree_depth(node_type *node) {
 	if(!node) return 0;
 
-	auto left = binary_tree_depth(node->left()) + 1;
-	auto right = binary_tree_depth(node->right()) + 1;
-
-	return max(left, right);
+	return 1 + max(
+		binary_tree_depth(node->left()),
+		binary_tree_depth(node->right())
+	);
 }
 
 int main() {
 	auto balanced = build_balanced_binary_tree();
+	print_binary_tree("balanced", balanced.get());
 	cout << "depth = " << binary_tree_depth(balanced.get())
 		<< endl;
 
 	auto unbalanced = build_unbalanced_binary_tree();
+	print_binary_tree("unbalanced", unbalanced.get());
 	cout << "depth = " << binary_tree_depth(unbalanced.get())
 		<< endl;
 
