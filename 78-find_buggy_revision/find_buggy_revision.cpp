471ca42332e18db6aa12038a8845df5a18101458
Marcelo Juchem <juchem at gmail dot com>
2012-05-25 00:46:30 -0700

    [78]: - fixing exercise - adding test cases

--- 78-find_buggy_revision/find_buggy_revision.cpp
+++ 78-find_buggy_revision/find_buggy_revision.cpp
@@ -8,30 +8,24 @@ template <typename T>
 int find_buggy_revision(int first, int last, T const &builder) {
 	assert(first < last);
 
-	bool buggy = false;
-
 	while(first < last) {
 		auto pivot = first + ((last - first) / 2);
 
 		if(builder.build(pivot)) {
 			first = pivot + 1;
-			buggy = true;
 		}
 		else {
 			last = pivot - 1;
-			buggy = false;
 		}
 	}
 
-	assert(buggy != builder.build(first));
-
-	return first + !buggy;
+	return first + builder.build(first);
 }
 
 class build_system {
 	int first;
-	int last;
 	int buggy;
+	int last;
 
 public:
 	build_system(int first, int buggy, int last):
@@ -59,23 +53,25 @@ int main() {
 	int first;
 	if(!(cin >> first)) return 1;
 
+	cout << "Build stream: ";
 	int buggy = first;
-	for(char outcome; (cin >> outcome) && outcome == 'g'; ++buggy);
-	int last = buggy;
-	for(char outcome; cin >> outcome; ++last); {
-		assert(outcome == 'b');
+	for(char outcome; cin >> outcome && outcome == 'g'; ++buggy) {
+		cout << outcome;
 	}
+	cout << 'b';
+	int last = buggy;
+	for(char outcome; cin >> outcome; ++last) {
+		assert(outcome == 'b');
+		cout << outcome;
+	}
+	cout << endl;
 
 	cout << "Previous build's revision (OK): " << first << endl;
 	cout << "Last build's revision (broken): " << last << endl;
 	cout << "Revision that broke the build: " << buggy << endl;
 
-/*
 	build_system builder(first, buggy, last);
 	auto result = find_buggy_revision(first, last, builder);
-/*/
-	auto result = find_buggy_revision(first, last, build_system(first, buggy, last));
-//*/
 
 	cout << "Result given by search function: " << result
 		<< (builder.did_introduce_bug(result) ? " correct" : " WRONG")
