b8aa2827d95951b1a8f5d7aa21ce85ce9ea8e8e4
Marcelo Juchem <juchem at gmail dot com>
2012-06-06 02:10:08 -0700

    simplifying exercise 18

--- 18-square_root/square_root.h
+++ 18-square_root/square_root.h
@@ -1,26 +1,19 @@
 #ifndef INCLUDED_square_root_h
 #define INCLUDED_square_root_h
 
+#include <algorithm>
+
+#include <cmath>
+
 template <typename T>
 T square_root(T n, T precision, unsigned &iterations) {
-	T min = 0;
-	T max = n;
 	T pivot = n;
 
 	iterations = 0;
 
-	for(T diff = precision; diff >= precision; ++iterations) {
-		pivot = min + ((max - min) / 2);
-		T result = pivot * pivot;
-
-		if(result < n) {
-			diff = n - result;
-			min = pivot;
-		}
-		else {
-			diff = result - n;
-			max = pivot;
-		}
+	for(T last = 0; precision < std::abs(pivot - last); ++iterations) {
+		last = pivot;
+		pivot = (pivot + (n / pivot)) / T(2);
 	}
 
 	return pivot;
